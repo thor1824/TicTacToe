47ecdf86e672156ab8fa42537a8cca85d31fee84
thor1824 <42994659+thor1824@users.noreply.github.com>
2018-10-08 14:26:16 +0200

    Update src/tictactoe/bll/GameBoard.java

---
+++ src/tictactoe/bll/GameBoard.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe.bll;
+
+/**
+ *
+ * @author Stegger
+ */
+public class GameBoard implements IGameModel
+{
+
+    /**
+     * Returns 0 for player 0, 1 for player 1.
+     *
+     * @return int Id of the next player.
+     */
+    public int getNextPlayer()
+    {
+        //TODO Implement this method
+        return 0;
+    }
+
+    /**
+     * Attempts to let the current player play at the given coordinates. It the
+     * attempt is succesfull the current player has ended his turn and it is the
+     * next players turn.
+     *
+     * @param col column to place a marker in.
+     * @param row row to place a marker in.
+     * @return true if the move is accepted, otherwise false. If gameOver == true
+     * this method will always return false.
+     */
+    public boolean play(int col, int row)
+    {
+        //TODO Implement this method
+        return true;
+    }
+
+    public boolean isGameOver()
+    {
+        //TODO Implement this method
+        return false;
+    }
+
+    /**
+     * Gets the id of the winner, -1 if its a draw.
+     *
+     * @return int id of winner, or -1 if draw.
+     */
+    public int getWinner()
+    {
+        //TODO Implement this method
+        return -1;
+    }
+
+    /**
+     * Resets the game to a new game state.
+     */
+    public void newGame()
+    {
+        //TODO Implement this method
+    }
+
+}
